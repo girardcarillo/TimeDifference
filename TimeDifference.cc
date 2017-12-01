7368cd72cc4445a0baa38d282e7764e841a4ea7a
Cloe Girard-Carillo <girardcarillo@lal.in2p3.fr>
2017-12-01 17:15:18 +0100

    Topology cut

--- TimeDifference.cc
+++ TimeDifference.cc
@@ -5,12 +5,13 @@
 // Third Party
 // - A
 // This Project
+// #include <datatools/bit_mask.h>
 #include <snemo/datamodels/data_model.h>
 #include <snemo/datamodels/topology_data.h>
 #include <mctools/simulated_data.h>
 #include <snemo/datamodels/topology_2e_pattern.h>
 
-// #include <sstream>
+#include <sstream>
 
 // #include <snemo/cuts/energy_measurement_cut.h>
 
@@ -42,10 +43,9 @@ void TimeDifference::initialize(const datatools::properties& setup_,
   dpp::base_module::_common_initialize(setup_);
   this->_set_initialized(true);
 
-  std::cout << "Enter output file name (ex.: flRec)" << std::endl;
-  std::cin >> _output_file_name_;
+  // std::cout << "Enter output file name (ex.: flRec)" << std::endl;
+  // std::cin >> _output_file_name_;
 }
-
 
 // Process
 dpp::base_module::process_status
@@ -53,15 +53,19 @@ TimeDifference::process(datatools::things& data_record_) {
   DT_THROW_IF(! is_initialized(), std::logic_error,
               "Module '" << get_name () << "' is not initialized !");
 
+
+
   ////Root export
-  // std::stringstream ss;
-  // ss.str("");
-  // ss << "sd_" << _output_file_name_;
-  // ss.str();
-  _sd_output_file_ = new TFile ("", "recreate", "Output file of Simulation data");
+  std::stringstream ss;
+  ss.str("");
+  ss << "sd_" << _output_file_name_;
+  ss.str();
+  _sd_output_file_ = new TFile ("sd_tree", "recreate", "Output file of Simulation data");
   _sd_output_file_->cd();
-  _sd_tree_= new TTree("calorimeter hit tree", "calorimeter hit tree");
+  _sd_tree_= new TTree("calorimeter_hit", "calorimeter_hit");
   _sd_tree_->Branch("time", &_time_,"time/D");
+
+
 
   ////Defining data base labels
   //td_label
@@ -70,8 +74,9 @@ TimeDifference::process(datatools::things& data_record_) {
   //sd_label
   const std::string & sd_label = snemo::datamodel::data_info::default_simulated_data_label();
 
-  ////Storing data bases
 
+
+  ////Storing data bases
   //Topology data base
   DT_THROW_IF(! data_record_.has("TD"), std::logic_error,
               "Data has no TD !");
@@ -88,31 +93,35 @@ TimeDifference::process(datatools::things& data_record_) {
   const genbb::primary_event::particles_col_type & the_primary_particles
     = a_primary_event.get_particles();
 
+
+
   ////Applying cuts on data bases
   //Cut on TD base
-    if (a_td.has_pattern() && a_td.has_pattern_as<snemo::datamodel::topology_2e_pattern>()) {
-      const snemo::datamodel::topology_2e_pattern & a_2e_topology
-        = a_td.get_pattern_as<snemo::datamodel::topology_2e_pattern>();
-      const double & a_energy_sum
-        = a_2e_topology.get_electrons_energy_sum();
-      if (a_energy_sum/CLHEP::MeV >= 2.7 && a_energy_sum/CLHEP::MeV <= 3.2) {
-        std::cout << "Cut on TD base OK" << std::endl;
-      }
+  if (a_td.has_pattern() && a_td.has_pattern_as<snemo::datamodel::topology_2e_pattern>()) {
+    const snemo::datamodel::topology_2e_pattern & a_2e_topology
+      = a_td.get_pattern_as<snemo::datamodel::topology_2e_pattern>();
+    const double & a_energy_sum
+      = a_2e_topology.get_electrons_energy_sum();
+    if (a_energy_sum/CLHEP::MeV >= 2.7 && a_energy_sum/CLHEP::MeV <= 3.2) {
+      std::cout << "Cut on TD base OK" << std::endl;
     }
+  }
 
-    //Cut on SD base
-    int count = 0;
-    for (const auto & iparticle : the_primary_particles) {
-      if (iparticle.is_electron()) {
-        count++;
-        // std::cout << "count = " << count << std::endl;
-      }
-      if (count == 2) {
-        std::cout << "Cut on SD base OK" << std::endl;
-      }
+  //Cut on SD base
+  int count = 0;
+  for (const auto & iparticle : the_primary_particles) {
+    if (iparticle.is_electron()) {
+      count++;
+      // std::cout << "count = " << count << std::endl;
     }
+    if (count == 2) {
+      std::cout << "Cut on SD base OK" << std::endl;
+    }
+  }
 
-    ////Storing data
+
+
+  ////Storing data
 
   // MUST return a status, see ref dpp::base_module::process_status
   return PROCESS_OK;
